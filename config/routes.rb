a2e1921c7c9d6778ab7db81384f7682dca4212e0
kenta.kawamoto <kawamoto.kenta74@gmail.com>
2020-03-22 21:54:17 +0000

    kadai sai teisyutu rm resources use

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create] 
+  # resources :users, only: [:index, :show, :new, :create] 
   
   resources :tasks
 #  resources :utasks, only: [:create, :destroy] 
