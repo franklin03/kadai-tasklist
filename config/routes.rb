1ab6d388aa139776a7377a554a50d1c2458aac58
kenta.kawamoto <kawamoto.kenta74@gmail.com>
2020-03-23 21:35:34 +0000

    fix sign up

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,8 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  # resources :users, only: [:index, :show, :new, :create] 
+#  resources :users, only: [:index, :show, :new, :create] 
+  resources :users, only: [:new, :create] 
   
   resources :tasks
 #  resources :utasks, only: [:create, :destroy] 
