81cea45c44e5f2812f2a98a0df196878c1f2f487
kenta.kawamoto <kawamoto.kenta74@gmail.com>
2020-03-24 21:55:53 +0000

    fix routes, add correct_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,12 @@
 Rails.application.routes.draw do
-  #get 'utasks/create'
-  #get 'utasks/destroy'
-  #get 'sessions/new'
-  #get 'sessions/create'
-  #get 'sessions/destroy'
-  # get 'logins/index'
-  # get 'users/index'
-  # get 'users/show'
-  # get 'users/new'
-  # get 'users/create'
-  # get 'toppages/index'
-  # root to: 'toppages#index'
   
   root to: 'tasks#index'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  get 'signup', to: 'users#new'
   delete 'logout', to: 'sessions#destroy'
   
-  get 'signup', to: 'users#new'
-#  resources :users, only: [:index, :show, :new, :create] 
   resources :users, only: [:new, :create] 
-  
   resources :tasks
-#  resources :utasks, only: [:create, :destroy] 
 end
