e656bb1e552683dc749c27bacf8b1a199f4b69f0
kenta.kawamoto <kawamoto.kenta74@gmail.com>
2020-03-26 21:21:38 +0000

    rm unnecessary code, fix some designs

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,12 +4,10 @@ class TasksController < ApplicationController
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    @task = current_user.tasks.build  # form_with 用
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
